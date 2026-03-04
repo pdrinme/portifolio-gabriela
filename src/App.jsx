190c4779ca52f1121ff33213c800769c80884262
pdrinme <pmenino28@gmail.com>
2026-03-04 15:48:05 -0300

    Add capacity data and icons; update App.jsx with LinkedIn contact section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import profileIcon from "./assets/profile.png"
+import likedinIcon from "./assets/likedin.png"
 import formation from "./formations.json"
+import capacity from "./capacitys.json"
 
 function App() {
 
@@ -9,12 +11,24 @@ function App() {
     <div class="dark:bg-[#181a1b] dark:text-white ">
       <section class="p-14">
         <header class="flex justify-center items-center border-2 border-gray-200 shadow-2xl rounded-4xl dark:border-[#363b3d]">
-          <div class="grid p-4 gap-4">
-            <div class="bg-linear-to-r/longer from-indigo-500 to-teal-400 p-1 rounded-[90rem]">
-              <img src={profileIcon} class="rounded-[100rem] w-2xs shadow-2xl " />
+          <div class="grid p-8 gap-6">
+            <div class="flex justify-center">
+              <div class="bg-linear-to-r/longer from-indigo-500 to-teal-400 p-1 rounded-[90rem] shadow-2xl transition hover:scale-110">
+                <img src={profileIcon} class="rounded-[100rem] w-2xs" />
+              </div>  
             </div>
             <h1 class="text-center text-4xl">Gabriela Emerick</h1>
             <p class="text-center text-xl">Idade: <span class="text-amber-300">19 anos</span></p>
+            <div class="flex flex-wrap gap-3 justify-center">
+              {
+                capacity.map((capacidades) =>
+                  <div class="flex p-2 gap-2 border-2 rounded-xl items-center shadow-2xl border-gray-200 bg-gray-100 transition dark:border-[#363b3d] dark:bg-[#181a1b] hover:bg-amber-500 hover:scale-110">
+                    <img src={capacidades.img} class="max-w-10"/>
+                    <p>{capacidades.name}</p>
+                  </div>
+                )
+              }
+            </div>
           </div>
         </header>
       </section>
@@ -62,6 +76,21 @@ function App() {
             </div>
           </div>
         </div>
+      </section>
+      <section >
+            <div class="grid p-5 gap-6">
+              <div class="grid gap-10 text-center justify-center">
+                <h2 class="text-2xl">Contato</h2>
+                <div class="border-2 border-gray-200 shadow-2xl rounded-xl p-4 text-center max-w-fit transition dark:border-[#363b3d] hover:scale-110 hover:bg-white hover:text-black">
+                  <a href="https://www.linkedin.com/in/gabriela-emerick/">
+                    <div class="flex items-center gap-2">
+                      <img src={likedinIcon} class="max-w-10" />
+                      <p>Likedin</p>
+                    </div>
+                  </a>
+                </div>
+              </div>
+            </div>
       </section>
     </div>
     </>
